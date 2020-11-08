1ade53e081a89836953f64d237a28b894d4a41ca
JerryMcDonald <jandh62808@gmail.com>
2020-11-08 10:49:22 -0600

    resume page now unnecessary

---
+++ my-app/src/views/ResumePage/ResumePage.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Document } from 'react-pdf/dist/entry.noworker';
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+// import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+// import People from "@material-ui/icons/People";
+
+// core components
+import Header from "../../components/Header/Header.js";
+import HeaderLinks from "../../components/Header/HeaderLinks.js";
+import Footer from "../../components/Footer/Footer.js";
+import GridContainer from "../../components/Grid/GridContainer.js";
+import GridItem from "../../components/Grid/GridItem.js";
+import Button from "../../components/CustomButtons/Button.js";
+import Card from "../../components/Card/Card.js";
+// import CardBody from "../../components/Card/CardBody.js";
+// import CardHeader from "../../components/Card/CardHeader.js";
+// import CardFooter from "../../components/Card/CardFooter.js";
+// import CustomInput from "../../components/CustomInput/CustomInput.js";
+
+import styles from "../../assets/jss/material-kit-react/views/loginPage.js";
+
+import image from "../../assets/img/bg3.jpg";
+
+import resume from "../../assets/img//pdfs/Jerry_McDonald_Resume.pdf"
+
+const useStyles = makeStyles(styles);
+
+export default function ResumePage(props) {
+  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  const [classicModal, setClassicModal] = React.useState(false);
+  setTimeout(function () {
+    setCardAnimation("");
+  }, 700);
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        absolute
+        color="transparent"
+        brand="Jerry McDonald"
+        rightLinks={<HeaderLinks />}
+        {...rest}
+      />
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: "url(" + image + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+        }}
+      >
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                  <embed
+    src={resume + "#toolbar=0"}
+    type="application/pdf"
+    height={600}
+    width={500}
+  />
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+        <Footer whiteFont />
+      </div>
+    </div>
+  );
+}
