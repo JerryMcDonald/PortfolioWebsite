435477708604c298e27e1bd5840e2024e9387844
JerryMcDonald <jandh62808@gmail.com>
2020-11-08 14:18:55 -0600

    create the contact js

---
+++ my-app/src/views/HomePage/Sections/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// core components
+import GridContainer from "../../../components/Grid/GridContainer.js";
+import GridItem from "../../../components/Grid/GridItem.js";
+import InfoArea from "../../../components/InfoArea/InfoArea.js";
+
+import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Contact() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Contact</h2>
+          <h5 className={classes.description}>
+            This is my contact page
+          </h5>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
