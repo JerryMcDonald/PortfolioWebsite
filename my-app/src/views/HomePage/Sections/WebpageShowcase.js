eaa234ae8116008d7a3af237203446ce571cd6d3
JerryMcDonald <jandh62808@gmail.com>
2020-11-15 22:00:04 -0600

    add a discription to operation spark to the webpageShowcase

--- my-app/src/views/HomePage/Sections/WebpageShowcase.js
+++ my-app/src/views/HomePage/Sections/WebpageShowcase.js
@@ -20,6 +20,7 @@ import GridContainer from "../../../components/Grid/GridContainer.js";
 import GridItem from "../../../components/Grid/GridItem.js";
 // import InfoArea from "../../../components/InfoArea/InfoArea.js";
 import NavPills from "../../../components/NavPills/NavPills.js";
+import Info from "../../../components/Typography/Info.js";
 
 // my images and icons
 // import operationSparkSVG from "../../../assets/img/icons/operationSparkSVG.svg";
@@ -34,14 +35,20 @@ import studio5 from "../../../assets/img/examples/studio-5.jpg";
 
 import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
 
+// bring in typography style
+import typoStyles from "../../../assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
+
 import showcaseStyles from "../../../assets/jss/material-kit-react/views/profilePage.js";
 
 const useStyles = makeStyles(styles);
+const useTypoStyles = makeStyles(typoStyles);
 
 const useShowcaseStyles = makeStyles(showcaseStyles);
 
 export default function WebpageShowcase() {
   const classes = useStyles();
+  const typoClasses = useTypoStyles();
+
   const showcaseClasses = useShowcaseStyles();
 
   const navImageClasses = classNames(
@@ -74,13 +81,38 @@ export default function WebpageShowcase() {
                   tabContent: (
                     <div>
                       <GridContainer justify="center">
-                        <GridItem xs={12} sm={12} md={8}>
+                        <GridItem xs={12} sm={12} md={16}>
                           <img alt=".." src={operationSpark} />
-                          <h5 className={classes.description}>
-                            I will have large image icons that will showcase the
-                            work that I completed in Bootcamp, and later I will
-                            showcase my work with C#/.Net
-                          </h5>
+                          <div className={typoClasses.section}>
+                          <div className={typoClasses.typo} style={{ paddingLeft: 0}}>
+                                <Info>
+                                  <p style={{ color: "gray"}}>
+                                  Operation Spark is a coding Bootcamp in New
+                                  Orleans, Louisiana. There are three phases of
+                                  the course: prep, bootcamp, and immersion. You
+                                  begin by being introduced to javascript, then
+                                  strengthen your skills through coding
+                                  challenges and projects. You transition to
+                                  front end web development using HTML, CSS,
+                                  React, and Vue, then to back-end API services
+                                  like Node.js, and finally database management
+                                  with MySQL and MongoDB. At the end of the
+                                  program, they pair you into groups with the
+                                  task of building webpages, programs, or games
+                                  with technologies of your choosing. I am proud
+                                  to have completed OPSpark, and I would be
+                                  happy to talk with anyone considering
+                                  the program. Just send me a message!
+                                  </p>
+                                </Info>
+                              </div>
+                          </div>
+                          <div>
+                            <p style={{ color: "black" }}>
+                              Here are the three webpages my team and I completed
+                              during Operation Spark.
+                            </p>
+                          </div>
                         </GridItem>
                       </GridContainer>
                       <GridContainer justify="center">
