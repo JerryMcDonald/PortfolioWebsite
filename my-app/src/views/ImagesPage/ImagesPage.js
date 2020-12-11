3f199bfb14b10f1525e98a1ce00e8ea151af9005
JerryMcDonald <jandh62808@gmail.com>
2020-12-10 21:11:40 -0600

    new images page.js

---
+++ my-app/src/views/ImagesPage/ImagesPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+import portfolioClip from "../../assets/img/assorted/portfolioClip.jpg";
+
+export default function ImagesPage() {
+  return (
+    <div>
+      <img src={portfolioClip} alt="..." />
+    </div>
+  );
+}
