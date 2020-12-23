5a296a6a5da2f6a6dbdbf94611bc1a6f88bfce7c
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2020-12-23 15:52:45 -0600

    clean up unused code

--- my-app/src/views/HomePage/Sections/WebpageShowcase.js
+++ my-app/src/views/HomePage/Sections/WebpageShowcase.js
@@ -1,76 +1,79 @@
 import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-// import SvgIcon from '@material-ui/core/SvgIcon';
-// import Icon from '@material-ui/core/Icon';
-
-// @fortawesome library
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 // nodejs library that concatenates classes
 import classNames from "classnames";
 
+import "../../../assets/css/styles.css";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+// import SvgIcon from '@material-ui/core/SvgIcon';
+// import Icon from '@material-ui/core/Icon';
+
+// @fortawesome library
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 import OperationSpark from "../Sections/OperationSpark.js";
+import Cpp from "../Sections/Cpp.js";
 
 // @material-ui/icons
-import Button from "../../../components/CustomButtons/Button.js";
+// import Button from "../../../components/CustomButtons/Button.js";
 // import Chat from "@material-ui/icons/Chat";
 // import VerifiedUser from "@material-ui/icons/VerifiedUser";
 // import Fingerprint from "@material-ui/icons/Fingerprint";
 // import Camera from "@material-ui/icons/Camera";
 // import CellWifiOutlined from "@material-ui/icons/CellWifiOutlined";
 // import Code from "@material-ui/icons/Code";
-import "../../../assets/css/styles.css";
+// import CSharp from "../../../assets/img/icons/CSharp.png";
+import CppIcon from "../../../assets/img/icons/C++_Icon.png";
 
 // core components
+// import Info from "../../../components/Typography/Info.js";
+// import InfoArea from "../../../components/InfoArea/InfoArea.js";
 import GridContainer from "../../../components/Grid/GridContainer.js";
 import GridItem from "../../../components/Grid/GridItem.js";
-// import InfoArea from "../../../components/InfoArea/InfoArea.js";
 import NavPills from "../../../components/NavPills/NavPills.js";
-import Info from "../../../components/Typography/Info.js";
 
 // my images and icons
 // import operationSparkSVG from "../../../assets/img/icons/operationSparkSVG.svg";
-import operationSpark from "../../../assets/img/OPSparkLogo.png";
-import Crawl from "../../../assets/img/Crawl.jpg";
-import crawlCrop from "../../../assets/img/showcase/crawlCrop.png";
-import crawlLogo from "../../../assets/img/showcase/crawlLogo.png";
+// import operationSpark from "../../../assets/img/OPSparkLogo.png";
+// import Crawl from "../../../assets/img/Crawl.jpg";
+// import crawlLogo from "../../../assets/img/showcase/crawlLogo.png";
+// import crawlCrop from "../../../assets/img/showcase/crawlCrop.png";
 // import CSharp from "../../../assets/img/icons/CSharp.png";
-
-// images
 // import studio1 from "../../../assets/img/examples/studio-1.jpg";
-import studio2 from "../../../assets/img/examples/studio-2.jpg";
+// import studio2 from "../../../assets/img/examples/studio-2.jpg";
 // import studio3 from "../../../assets/img/examples/studio-3.jpg";
 // import studio4 from "../../../assets/img/examples/studio-4.jpg";
-import studio5 from "../../../assets/img/examples/studio-5.jpg";
-import dotNetCore from "../../../assets/img/icons/dotNetCore.png";
+// import studio5 from "../../../assets/img/examples/studio-5.jpg";
+// import dotNetCore from "../../../assets/img/icons/dotNetCore.png";
 
+// styles
+// import iconStyles from "../../../assets/jss/material-kit-react/views/componentsSections/downloadStyle.js";
+// import typoStyles from "../../../assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
+// import showcaseStyles from "../../../assets/jss/material-kit-react/views/profilePage.js";
 import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
 
-// bring in typography style
-import typoStyles from "../../../assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
-// bring in icon style
-// import iconStyles from "../../../assets/jss/material-kit-react/views/componentsSections/downloadStyle.js";
-
-import showcaseStyles from "../../../assets/jss/material-kit-react/views/profilePage.js";
-
-const useStyles = makeStyles(styles);
-const useTypoStyles = makeStyles(typoStyles);
+// const useTypoStyles = makeStyles(typoStyles);
 // const useIconStyles = makeStyles(iconStyles);
-const useShowcaseStyles = makeStyles(showcaseStyles);
+// const useShowcaseStyles = makeStyles(showcaseStyles);
+const useStyles = makeStyles(styles);
+const useStyles2 = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function WebpageShowcase() {
-  const classes = useStyles();
-  const typoClasses = useTypoStyles();
+  // const typoClasses = useTypoStyles();
   // const iconClasses = useIconStyles();
-  const showcaseClasses = useShowcaseStyles();
-
-  const navImageClasses = classNames(
-    showcaseClasses.imgRounded,
-    showcaseClasses.imgGallery
-  );
+  const classes = useStyles();
+  const iconClasses = useStyles2();
 
   return (
     <div className={classes.section}>
@@ -86,7 +89,7 @@ export default function WebpageShowcase() {
       </GridContainer>
       <div>
         <GridContainer justify="center">
-          <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+          <GridItem xs={18} sm={18} md={10} className={classes.navWrapper}>
             <NavPills
               alignCenter
               color="rose"
@@ -102,6 +105,24 @@ export default function WebpageShowcase() {
                   tabContent: (
                     <div>
                       <OperationSpark />
+                    </div>
+                  ),
+                },
+                {
+                  tabButton: (
+                    <div className={iconClasses.root}>
+                      <Avatar
+                        variant="square"
+                        className={classes.square}
+                        src={CppIcon}
+                        title="C#"
+                      />
+                    </div>
+                  ),
+                  // tabIcon: Code,
+                  tabContent: (
+                    <div>
+                      <Cpp />
                     </div>
                   ),
                 },
