a0be2709b9c5f8c18344f4827ce3a576558c5927
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2020-12-23 15:53:04 -0600

    Cpp in it's current state. Needs working links

---
+++ my-app/src/views/HomePage/Sections/Cpp.js
@@ -0,0 +1,258 @@
+import React from "react";
+// import Carousel from "react-slick";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import "../../../assets/css/styles.css";
+
+// material-ui components
+// import MuiDialogTitle from "@material-ui/core/DialogTitle";
+// import IconButton from "@material-ui/core/IconButton";
+// import CloseIcon from "@material-ui/icons/Close";
+// import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+// components
+import GridContainer from "../../../components/Grid/GridContainer.js";
+import GridItem from "../../../components/Grid/GridItem.js";
+import Card from "../../../components/Card/Card.js";
+import CardHeader from "../../../components/Card/CardHeader.js";
+import Quote from "../../../components/Typography/Quote.js";
+
+// images and icons
+import CppBlogBannerOne from "../../../assets/img/blog/CppBlogBannerOne.png";
+import CppBlogBannerTwo from "../../../assets/img/blog/CppBlogBannerTwo.png";
+
+// import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+// import breakawayStyles from "../../../assets/jss/material-kit-react/views/componentsSections/breakawayStyle.js";
+// import showcaseStyles from "../../../assets/jss/material-kit-react/views/profilePage.js";
+// import carouselStyle from "../../../assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
+// import typoStyles from "../../../assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
+import blogsStyle from "../../../assets/jss/material-kit-pro-react/views/sectionsSections/blogsStyle.js";
+
+// const useStyles = makeStyles(blogsStyle);
+// const useShowcaseStyles = makeStyles(showcaseStyles);
+// const useTypoStyles = makeStyles(typoStyles);
+// const useCarouselStyles = makeStyles(carouselStyle);
+const useStyles = makeStyles(blogsStyle);
+
+export default function Cpp() {
+  // const typoClasses = useTypoStyles();
+  // const carouselClasses = useCarouselStyles();
+  const classes = useStyles();
+
+  return (
+    <div>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={16}>
+          <Quote
+            text="“C++ is designed to allow you to express ideas, but if you don't have ideas or don't have any clue about how to express them, C++ doesn't offer much help.”"
+            author=" Bjarne Stroustrup"
+          />
+        </GridItem>
+      </GridContainer>
+      <Divider variant="middle" />
+      {/* Blogs START */}
+      <div className={classes.blog}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem
+              xs={12}
+              sm={10}
+              md={10}
+              className={classes.mlAuto + " " + classes.mrAuto}
+            >
+              <h2 className={classes.title}>Blogposts</h2>
+              <br />
+              <Card plain blog className={classes.card}>
+                <GridContainer>
+                  <GridItem xs={12} sm={5} md={5}>
+                    <CardHeader image plain>
+                      <a href="#pablito" onClick={(e) => e.preventDefault()}>
+                        <img src={CppBlogBannerTwo} alt="..." />
+                      </a>
+                      <div
+                        className={classes.coloredShadow}
+                        style={{
+                          backgroundImage: `url(${CppBlogBannerTwo})`,
+                          opacity: "1",
+                        }}
+                      />
+                    </CardHeader>
+                  </GridItem>
+                  <GridItem xs={12} sm={7} md={7}>
+                    <h3 className={classes.cardTitle}>
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        C++ Variables, Functions, Conditionals, and Logic. In
+                        VSCode.
+                      </a>
+                    </h3>
+                    <p className={classes.description1}>
+                      In this series, I am reviewing the C++ basics, primarily
+                      for those who already understand a coding language and are
+                      curious about what makes C++ different. — and…
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        {" "}
+                        Read More{" "}
+                      </a>
+                    </p>
+                  </GridItem>
+                </GridContainer>
+              </Card>
+              <Card plain blog className={classes.card}>
+                <GridContainer>
+                  <GridItem xs={12} sm={7} md={7}>
+                    <h3 className={classes.cardTitle}>
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        Create a C++ program. Run in Visual Studio Code.{" "}
+                      </a>
+                    </h3>
+                    <p className={classes.description1}>
+                      Do you think that C++ is too old school to learn? Think
+                      again! It is still a highly sought after skill. Learning
+                      C++ will better prepare you for learning C# and give you a
+                      broader skill set! — and…
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        {" "}
+                        Read More{" "}
+                      </a>
+                    </p>
+                  </GridItem>
+                  <GridItem xs={12} sm={5} md={5}>
+                    <CardHeader image plain>
+                      <a href="#pablito" onClick={(e) => e.preventDefault()}>
+                        <img src={CppBlogBannerOne} alt="..." />
+                      </a>
+                      <div
+                        className={classes.coloredShadow}
+                        style={{
+                          backgroundImage: `url(${CppBlogBannerOne})`,
+                          opacity: "1",
+                        }}
+                      />
+                    </CardHeader>
+                  </GridItem>
+                </GridContainer>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
+      {/* Blogs END */}
+      {/* Project START */}
+      {/* <div className={classes.projects + " " + classes.projects4}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem
+              xs={12}
+              sm={8}
+              md={8}
+              className={
+                classes.mlAuto + " " + classes.mrAuto + " " + classes.textCenter
+              }
+            >
+              <h2 className={classes.title}>
+                Some of Our Awesome Products - 4
+              </h2>
+              <h5 className={classes.description}>
+                This is the paragraph where you can write more details about
+                your projects. Keep you user engaged by providing meaningful
+                information.
+              </h5>
+              <div className={classes.sectionSpace} />
+            </GridItem>
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
+              <Card
+                background
+                className={classes.card4}
+                style={{ backgroundImage: `url(${cardProject2})` }}
+              >
+                <CardBody background className={classes.cardBody4}>
+                  <Badge color="rose">Client: Apple</Badge>
+                  <a href="#pablo" onClick={e => e.preventDefault}>
+                    <h3 className={classes.cardTitle}>Beautiful Project</h3>
+                    <p className={classes.cardDescription}>
+                      Don{"'"}t be scared of the truth because we need to
+                      restart the human foundation in truth.
+                    </p>
+                  </a>
+                </CardBody>
+              </Card>
+            </GridItem>
+            <GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
+              <InfoArea
+                className={classes.info4}
+                title="Graphic Design"
+                description="We've created the design of the website using modern Technologies like Sketch. It was a very interesting collaboration."
+                icon={FormatPaint}
+                iconColor="info"
+              />
+              <InfoArea
+                className={classes.info4}
+                title="Fully Coded in HTML5"
+                description="We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub."
+                icon={Code}
+                iconColor="primary"
+              />
+              <InfoArea
+                className={classes.info4}
+                title="CMS Admin Dashboard"
+                description="There is also a Fully Customizable CMS Admin Dashboard for this product."
+                icon={Dashboard}
+                iconColor="danger"
+              />
+            </GridItem>
+          </GridContainer>
+          <hr />
+          <GridContainer>
+            <GridItem xs={12} sm={5} md={5} className={classes.mlAuto}>
+              <InfoArea
+                className={classes.info4}
+                title="Marketing"
+                description="We've created the marketing campaign of the website. It was a very interesting collaboration."
+                icon={Timeline}
+                iconColor="rose"
+              />
+              <InfoArea
+                className={classes.info4}
+                title="Fully Coded in HTML5"
+                description="We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub."
+                icon={Code}
+                iconColor="success"
+              />
+              <InfoArea
+                className={classes.info4}
+                title="Built Audience"
+                description="There is also a Fully Customizable CMS Admin Dashboard for this product."
+                icon={Group}
+                iconColor="info"
+              />
+            </GridItem>
+            <GridItem xs={12} sm={5} md={5} className={classes.mrAuto}>
+              <Card
+                background
+                className={classes.card4}
+                style={{ backgroundImage: `url(${cardProject5})` }}
+              >
+                <CardBody background className={classes.cardBody4}>
+                  <Badge color="rose">CLIENT: DJ KHALED</Badge>
+                  <a href="#pablo" onClick={e => e.preventDefault}>
+                    <h3 className={classes.cardTitle}>Another One</h3>
+                    <p className={classes.cardDescription}>
+                      Don{"'"}t be scared of the truth because we need to
+                      restart the human foundation in truth.
+                    </p>
+                  </a>
+                </CardBody>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div> */}
+      {/* Projects END */}
+      <Divider variant="middle" />
+    </div>
+  );
+}
