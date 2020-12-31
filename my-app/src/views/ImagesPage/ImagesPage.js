3e85c9c5e01ca1febf3255e628f5d327366e31be
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2020-12-31 15:00:20 -0600

    find image for meta tag

--- my-app/src/views/ImagesPage/ImagesPage.js
+++ my-app/src/views/ImagesPage/ImagesPage.js
@@ -3,11 +3,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 import portfolioClip from "../../assets/img/assorted/portfolioClip.jpg";
+import meta from "../../assets/img/metaImages/newMainPageMetaImage.png";
+
 
 export default function ImagesPage() {
   return (
     <div>
-      <img src={portfolioClip} alt="..." />
+      <img src={meta} alt="..." />
     </div>
   );
 }
